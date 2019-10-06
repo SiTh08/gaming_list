2d3103936a458b12f905d5728cb55999cb9b385d
Sivaharan Thevipagan <SThevipagan@spartaglobal.com>
2019-10-06 21:09:29 +0100

    made the method to create one game

--- functions.py
+++ functions.py
@@ -10,3 +10,14 @@ class functions():
         self.password = password
         self.conn_rdb = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + self.server + ';DATABASE=' + self.database + ';UID=' + self.username + ';PWD=' + self.password)
         self.cursor = self.conn_rdb.cursor()
+
+    def execute(self, query):
+        return self.cursor.execute(query)
+
+# As a user, I can create one game.
+
+    def create(self, name, genre, platform, price, phone, location):
+        query = f'INSERT INTO Games([Name], Genre, Platforms, Price, Phone, [Location]) VALUES ({name}, {genre}, {platform}, {price}, {phone}, {location})'
+        self.execute(query)
+        self.conn_rdb.commit()
+
